4083e4751fa456dc5b981d498f149f9615cb7d20
Niels_Overkamp <niels.overkamp@gmail.com>
2017-03-07 21:58:31 +0100

    Auto saving and loading position & waypoints

--- AdvancedRobotMovementSystem/lib/arms.lua
+++ AdvancedRobotMovementSystem/lib/arms.lua
@@ -2,73 +2,126 @@ local rob = require('robot')
 
 local api = {}
 
-api.direction = 0
-api.x = 0
-api.y = 0
-api.z = 0
+api.pos = {
+  direction = 0,
+  x = 0,
+  y = 0,
+  z = 0,
+  path = '/home/pos.ser'
+}
 
-api.waypoints = {}
+
+api.waypoints = {
+  path = '/home/waypoints.ser'
+}
+
+
+function api:load(object)
+  if not require('filesystem').exists(object.path) then return object end
+  local file = assert(io.open(object.path, 'r'))
+  local text = file:read('*all') or ''
+  file:close()
+  return require('serialization').unserialize(text) or object
+end
+
+function api:loadPos()
+  self.pos = self:load(self.pos)
+end
+
+function api:loadWaypoints()
+  self.waypoints = self:load(self.waypoints)
+end
+
+
+function api:save(object)
+  local text = require('serialization').serialize(object) or ''
+  local file = assert(io.open(object.path, 'w'))
+  file:write(text)
+  file:close()
+end
+
+function api:savePos()
+  self:save(self.pos)
+end
+
+function api:saveWaypoints()
+  self:save(self.waypoints)
+end
+
+
 
 function api:forward(distance)
   distance = distance or 1
-  if self.direction == 0 then
-    self.x = self.x + distance
-  elseif self.direction == 1 then
-    self.y = self.y + distance
-  elseif self.direction == 2 then
-    self.x = self.x - distance
+  if self.pos.direction == 0 then
+    self.pos.x = self.pos.x + distance
+  elseif self.pos.direction == 1 then
+    self.pos.y = self.pos.y + distance
+  elseif self.pos.direction == 2 then
+    self.pos.x = self.pos.x - distance
   elseif self.direction == 3 then
-    self.y = self.y - distance
+    self.pos.y = self.pos.y - distance
   end
 
   for i = 1, distance do
     rob.forward()
   end
+
+  self:savePos()
 end
 
 function api:back(distance)
   distance = distance or 1
-  if self.direction == 0 then
-    self.x = self.x - distance
-  elseif self.direction == 1 then
-    self.y = self.y - distance
-  elseif self.direction == 2 then
-    self.x = self.x + distance
-  elseif self.direction == 3 then
-    self.y = self.y + distance
+  if self.pos.direction == 0 then
+    self.pos.x = self.pos.x - distance
+  elseif self.pos.direction == 1 then
+    self.pos.y = self.pos.y - distance
+  elseif self.pos.direction == 2 then
+    self.pos.x = self.pos.x + distance
+  elseif self.pos.direction == 3 then
+    self.pos.y = self.pos.y + distance
   end
 
   for i = 1, distance do
     rob.back()
   end
+
+  self:savePos()
 end
 
 function api:up(distance)
   distance = distance or 1
-  self.z = self.z + distance
+  self.pos.z = self.pos.z + distance
 
   for i = 1, distance do
-  rob.up()
+    rob.up()
   end
+
+  self:savePos()
 end
 
 function api:down(distance)
   distance = distance or 1
-  self.z = self.z - distance
+  self.pos.z = self.pos.z - distance
 
   for i = 1, distance do
-  rob.down()
+    rob.down()
   end
+
+  self:savePos()
 end
 
 function api:turnLeft()
-  self.direction = (self.direction - 1) % 4
+  self.pos.direction = (self.pos.direction - 1) % 4
   rob.turnLeft()
+
+  self:savePos()
 end
 
 function api:turnRight()
-  self.direction = (self.direction + 1) % 4
+  self.pos.direction = (self.pos.direction + 1) % 4
   rob.turnRight()
+
+  self:savePos()
 end
 
 api.fd = api.forward
@@ -78,15 +131,16 @@ api.lt = api.turnLeft
 api.rt = api.turnRight
 
 function api:addWaypoint(x, y, z)
-  x = x or self.x
-  y = y or self.y
-  z = z or self.z
+  x = x or self.pos.x
+  y = y or self.pos.y
+  z = z or self.pos.z
 
   waypoints[#waypoints + 1] = {
     x = x,
     y = y,
     z = z
   }
+  self:saveWaypoints()
 end
 
 function api:goTo(waypoint, y, z)
@@ -96,16 +150,22 @@ function api:goTo(waypoint, y, z)
     y = waypoint.z
   elseif typeof(waypoint) == 'number' then
     x = waypoint
-    y = y or self.y
-    z = z or self.z
+    y = y or self.pos.y
+    z = z or self.pos.z
   else
-    error('invalid call')
+    x = x or self.pos.x
+    y = y or self.pos.y
+    z = z or self.pos.z
   end
 
 
 end
 
-api["goto"] = api.goTo
+api['goto'] = api.goTo
+
+
+api:loadPos()
+api:loadWaypoints()
 
 
 return api
