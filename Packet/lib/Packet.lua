b29d72c18bc74535b224a20df0328da9122d1641
Niels_Overkamp <niels.overkamp@gmail.com>
2017-03-28 15:09:34 +0200

    Added a readFromString function to Packet.lua
    readFromString takes a string, converts it to a byteArray and fills in
    the fields of the packet in order bit for bit

--- Packet/lib/Packet.lua
+++ Packet/lib/Packet.lua
@@ -27,6 +27,10 @@ function Packet.NumberField32:getBitAt(disp)
   return bit32.band(bit32.rshift(self.number, disp),1)
 end
 
+function Packet.NumberField32:setBitAt(disp, value)
+  self.number = self.number + bit32.lshift(bit32.band(value,1),disp)
+end
+
 function Packet.NumberField32:length()
   return self.lengthInBits
 end
@@ -49,7 +53,6 @@ function Packet:getStringRepresentation()
     for i = 0, (field:length() - 1) do
       currentCharInByte = currentCharInByte +
         bit32.lshift(field:getBitAt(i),bitPointer)
-      print(bitPointer, i, currentCharInByte)
       bitPointer = bitPointer + 1
       if bitPointer >= 8 then
         bitPointer = 0
@@ -63,11 +66,25 @@ function Packet:getStringRepresentation()
 end
 
 function Packet:readFromString(packetString)
+  print("bitP","i","number")
   byteArray = {string.byte(packetString, 1, #packetString)}
   charPointer = 1
   bitPointer = 0
-  for _, field in ipairs(self.field) do
-    for i = 0, field:length() do
+  for _, field in ipairs(self.fields) do
+    for i = 0, (field:length()-1) do
+      field:setBitAt(i, bit32.rshift(byteArray[charPointer], bitPointer))
+      bitPointer = bitPointer + 1
+      print(bitPointer, i, field.number)
+      if bitPointer >= 8 then
+        bitPointer = 0
+        charPointer = charPointer + 1
+        if (charPointer > #byteArray) then
+          return
+        end
+      end
+    end
+  end
+end
 
 
 
